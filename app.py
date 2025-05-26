0bf211853ca9c4915b5a4f1bc880e58f9ce15617
Nir Haroush <nirty14@gmail.com>
2025-05-26 20:31:34 +0300

    secure changes v3

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ import random
 import string
 from server_logic import apiRegister
 from server_logic import login as APIlogin
+import bleach
 
+def sanitize(input_str):
+    return bleach.clean(input_str, tags=[], attributes={}, strip=True)
 
 #test
 load_dotenv()
@@ -132,11 +135,11 @@ def systemScreen():
 @app.route('/add_customer', methods=['POST'])
 def add_customer():
     data = request.get_json()
-    first_name = data.get('firstName')
-    last_name = data.get('lastName')
-    phone = data.get('phone')
-    bday = data.get('bday')
-    email = data.get('email')
+    first_name = sanitize(data.get('firstName', ''))
+    last_name = sanitize(data.get('lastName', ''))
+    phone = sanitize(data.get('phone', ''))
+    bday = sanitize(data.get('bday', ''))
+    email = sanitize(data.get('email', ''))
 
     conn = get_db_connection()
     cursor = conn.cursor()
